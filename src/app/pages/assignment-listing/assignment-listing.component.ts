d043e0f1f723554a708c03531001564d41190287
Thulasi k <thulasik112001@gmail.com>
2024-07-11 16:57:02 +0530

    bugfix/listing pages

--- src/app/pages/assignment-listing/assignment-listing.component.ts
+++ src/app/pages/assignment-listing/assignment-listing.component.ts
@@ -3,61 +3,62 @@ import { ButtonComponent } from "../../components/button/button.component";
 import { ListingCardComponent } from "../../components/batch-listing-card/batch-listing-card.component";
 import { DropdownComponent } from "../../components/dropdown/dropdown.component";
 import { AssignmentListingCardComponent } from "../../components/assignment-listing-card/assignment-listing-card.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-assignment-listing',
     standalone: true,
     templateUrl: './assignment-listing.component.html',
     styleUrl: './assignment-listing.component.scss',
-    imports: [ButtonComponent, ListingCardComponent, DropdownComponent, AssignmentListingCardComponent]
+    imports: [ButtonComponent, ListingCardComponent, DropdownComponent, AssignmentListingCardComponent,CommonModule]
 })
 export class AssignmentListingComponent {
-  pendingAssignments = [
+  Assignments = [
     {
       assessment_title: 'GG Hospital',
-      post_date: '20-June 2024',
-      due_date: '11/10/2024',
-      totalCountOfTrainees: '38',
-      totalSubmits:'21',
-    },
-    {
-      assessment_title: 'Portfolio',
-      post_date: '18-June 2024',
-      due_date: '20-June-2024 11:00 PM',
-      totalCountOfTrainees: '38',
-      totalSubmits: '3',
-    },
-    {
-      assessment_title: 'Case Study',
-      post_date: '1-June 2024',
-      due_date: '13-June-2024 11:00 PM',
-      totalCountOfTrainees: '38',
-      totalSubmits: '7',
-    }
-  ];
-
-  completedAssessments = [
-    {
-      assessment_title: 'GG Hospital',
-      post_date: '20-June 2024',
-      due_date: '23-June-2024 11:00 PM',
+      post_date: new Date('2024-06-20'),
+      due_date: new Date('2024-10-11'),
       totalCountOfTrainees: '38',
       totalSubmits: '21',
     },
     {
       assessment_title: 'Portfolio',
-      post_date: '18-June 2024',
-      due_date: '20-June-2024 11:00 PM',
+      post_date: new Date('2024-06-18'),
+      due_date: new Date('2024-06-20T23:00:00'),
+      totalCountOfTrainees: '38',
+      totalSubmits: '3',
+    },
+    {
+      assessment_title: 'Case Study',
+      post_date: new Date('2024-06-01'),
+      due_date: new Date('2024-06-13T23:00:00'),
+      totalCountOfTrainees: '38',
+      totalSubmits: '7',
+    },
+    {
+      assessment_title: 'GG Hospital',
+      post_date: new Date('2024-06-20'),
+      due_date: new Date('2024-06-23T23:00:00'),
+      totalCountOfTrainees: '38',
+      totalSubmits: '21',
+    },
+    {
+      assessment_title: 'Portfolio',
+      post_date: new Date('2024-06-18'),
+      due_date: new Date('2024-06-20T23:00:00'),
       totalCountOfTrainees: '38',
       totalSubmits: '3',
     },
     {
       assessment_title: 'Portfolio',
-      post_date: '18-June 2024',
-      due_date: '20-June-2024 11:00 PM',
+      post_date: new Date('2024-06-18'),
+      due_date: new Date('2024-06-20T23:00:00'),
       totalCountOfTrainees: '38',
       totalSubmits: '3',
     },
   ];
+
+  todayDate: Date = new Date();
+
   constructor(){}
 }

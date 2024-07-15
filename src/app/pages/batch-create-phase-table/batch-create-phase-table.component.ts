4480cb56bbf9ba3e683bc4b07fe8b5c135bdae66
Thulasi k <thulasik112001@gmail.com>
2024-07-15 12:26:11 +0530

    batch creation page test commit

---
+++ src/app/pages/batch-create-phase-table/batch-create-phase-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-batch-create-phase-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './batch-create-phase-table.component.html',
+  styleUrl: './batch-create-phase-table.component.scss'
+})
+export class BatchCreatePhaseTableComponent {
+
+}

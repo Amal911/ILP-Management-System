6290f6597aa7a6ed522bf0ed7ffd2cd604ebc7f5
Thulasi k <thulasik112001@gmail.com>
2024-07-15 11:08:56 +0530

    batch creation page initial commit

---
+++ src/app/components/batch-creation-table/batch-creation-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-batch-creation-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './batch-creation-table.component.html',
+  styleUrl: './batch-creation-table.component.scss'
+})
+export class BatchCreationTableComponent {
+
+}

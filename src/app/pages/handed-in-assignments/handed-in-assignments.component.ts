1473f7b6f28c863cd236cf9825335cc28a5d0708
Jisna George <jisna.george@experionglobal.local>
2024-07-15 09:57:38 +0530

    Assignments trainee submission trainee view completed

--- src/app/pages/handed-in-assignments/handed-in-assignments.component.ts
+++ src/app/pages/handed-in-assignments/handed-in-assignments.component.ts
@@ -1,36 +1,69 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { FileUpload, FileUploadEvent, FileUploadModule } from 'primeng/fileupload';
+import { ToastModule } from 'primeng/toast';
+interface UploadEvent {
+  originalEvent: Event;
+  files: File[];
+}
 
 @Component({
   selector: 'app-handed-in-assignments',
   standalone: true,
-  imports: [CommonModule,FormsModule,ReactiveFormsModule],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule,FileUploadModule,ToastModule],
   templateUrl: './handed-in-assignments.component.html',
-  styleUrl: './handed-in-assignments.component.scss'
+  styleUrl: './handed-in-assignments.component.scss',
+  providers: [MessageService],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HandedInAssignmentsComponent {
 
   workForm: FormGroup;
   submitted: boolean = false;
   
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,private messageService: MessageService) {
     this.workForm = this.fb.group({
-      link: ['', [Validators.required, Validators.pattern('https?://.+')]],
-      comments: ['', Validators.required]
+      links: this.fb.array([this.createLink()]),
+      comments: ['', Validators.required],
+      fileUpload: ['']
+
     });
   }
+  createLink(): FormGroup {
+    return this.fb.group({
+      link: ['', [Validators.required, Validators.pattern('https?://.+')]]
+    });
+  }
+
+  get links(): FormArray {
+    return this.workForm.get('links') as FormArray;
+  }
+
+  addLink(): void {
+    this.links.push(this.createLink());
+  }
+
+  removeLink(index: number): void {
+    this.links.removeAt(index);
+  }
+
   onSubmit() {
+    debugger
     
-    if (this.workForm.valid) {
-      console.log(this.workForm.value);
-    }
+      console.log({
+        ...this.workForm.value,
+        uploadedFiles: this.uploadedFiles
+      });
+      this.submitted = true;
    
   }
   show(){
     this.submitted = true;
 
   }
+  
 
   onCancel() {
     if (this.workForm.valid) {
@@ -39,6 +72,15 @@ export class HandedInAssignmentsComponent {
       this.submitted = false;
     }
   }
-  
+  uploadedFiles: any[] = [];
+
+
+  onUpload(event:FileUploadEvent) {
+    for(let file of event.files) {
+        this.uploadedFiles.push(file);
+    }
+
+    this.messageService.add({severity: 'info', summary: 'File Uploaded', detail: ''});
+}
   
 }
